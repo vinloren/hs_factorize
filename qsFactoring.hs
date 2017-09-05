f4d252797b117ae6fca8505ec2148ce2638a1853
Vincenzo Lorenzale <vinloren@gmail.com>
2017-09-05 17:24:00 +0000

    added auto sizing of B smooth primes and auto build of semi-prime

--- qsFactoring.hs
+++ qsFactoring.hs
@@ -2,7 +2,6 @@ import Factors
 import System.Random
 import Data.Time
 -- import Data.Time.Clock.POSIX
--- import System.IO.Unsafe
 -- import System.Time
 
 findPrime p = if (testp p) == [0] then p else findPrime (p+2)
@@ -15,56 +14,88 @@ testa :: [Integer] -> Integer
 testa []     = 0
 testa xs = head xs
 
+ceil :: Float -> Integer
+ceil x = ceiling(x)
+
 -- Factorize semi prime with Quadratic Sieving method
 
+getopt = do 
+  putStrLn "1) gimme semi-prime\n2) generate semi-prime"
+  s <-getLine
+  if s == "1" 
+    then do 
+      putStrLn "Gimme semi-prime"
+      inp <- getLine
+      let n = read(inp)
+      return n
+    else if s /= "2"
+      then getopt
+      else do
+       putStrLn "Decimal digits for the two primes to be generated?"
+       inp <- getLine
+       let dig = read(inp)   
+       let b1 = 10^dig
+       let b2 = 10^(dig-2)+b1
+       nu <- randomRIO(b1, b2)
+       let p = if nu  `mod` 2 == 1 then nu else nu + 1
+       let fact1 = findPrime p
+       nu <- randomRIO(b1, b2)
+       let q =  if nu `mod` 2 == 1 then nu else nu + 1
+       let fact2 = findPrime q
+       let n = fact1*fact2
+       return n
+
+
 main = do
-  putStrLn "Decimal digits for the two primes to be generated?"
-  inp <- getLine
-  let dig = read(inp)
-  nu <- randomRIO(10^dig, 10^(dig+1)-1)
-  let p = if nu  `mod` 2 == 1 then nu else nu + 1
-  let fact1 = findPrime p
-  nu <- randomRIO(10^dig, 10^(dig+1)-1)
-  let q =  if nu `mod` 2 == 1 then nu else nu + 1
-  let fact2 = findPrime q
-  let n = fact1*fact2
-  putStr "Semi prime: "
-  print (show n)
-  putStrLn "B primes size?"
-  inp <- getLine
-  let bsz = read(inp)
-  let bp = filtB (fndPrimes [2..bsz] []) n
-  print bp
-  start <- getCurrentTime
-  let r = radix n 2 0
-  let scn = scany n r bp 1 (2*r) (length bp) []
-  putStrLn "Scanned sieve:"
-  print (length(scn))
-  print scn
-  let p2 = pow2 (toInteger(length(bp))-1) []
+ n <- getopt
+ putStr "Semi prime: "
+ print (show n)
+ let b = log (fromIntegral n)
+ let sqb = 0.5 * sqrt (b)
+ let lgb = sqrt (log (b))
+-- print lgb
+ let bsz = round (exp (sqb * lgb))
+ putStr "Max B : "
+ print bsz
+ let bp = filtB (fndPrimes [2..bsz] []) n
+ print bp
+ start <- getCurrentTime
+ let r = radix n 2 0
+ let scn = scany n r bp 1 (2*r) (length bp) []
+ putStrLn "Scanned sieve:"
+ print (length(scn))
+ print scn
+ siev <- getCurrentTime
+ putStr "Scan done in "
+ print (diffUTCTime siev start)
+ let ln = toInteger(length bp)
+ let p2 = pow2 (ln-1) []
 -- print p2
-  let bf = getBl scn p2 []
-  let rs = replE scn bf []
+ let bf = getBl scn p2 []
+ let rs = replE scn bf []
 --  putStrLn "r, y, Exp binary matrix:"
 --  print rs
 --  putStrLn "Sorted scn :"
-  let srt3 = sort3 rs (0,0,(-1)) []
+ let srt3 = sort3 rs (0,0,(-1)) []
 --  print srt3
-  let sol = rgs srt3 0 (length(scn)-1) (length bp) (length(scn)-1)
+ let sol = rgs srt3 0 (length(scn)-1) (length bp) (length(scn)-1)
 --  putStrLn "Solution:"
 --  print sol
 --  let fin = sols sol 0 0  (length bp)
 --  print fin
-  putStr ("N. solutions: ")
-  print (length sol)
-  let rslt = resolv sol n
-  putStr ("found (p,q): ")
-  print rslt
-  end <- getCurrentTime
-  putStr "Runnin time (secs.) = "
-  print (diffUTCTime end start)
+ putStr ("N. solutions: ")
+ print (length sol)
+ red <- getCurrentTime
+ putStr "Matrix reduced in "
+ print (diffUTCTime red siev)
+-- print sol
+ let rslt = (resolv sol n)
+ end <- getCurrentTime
+ putStr "Factors found in "
+ print (diffUTCTime end start)
+ putStr "found (p,q): "
+ print (show(rslt))
 
-  
-  
-  
-  
+
+       
+
