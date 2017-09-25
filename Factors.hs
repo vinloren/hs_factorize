448f81242df2311f0786ad5aa9e151fde5a5d0b1
Vincenzo Lorenzale <vinloren@gmail.com>
2017-09-25 16:41:22 +0000

    consolidated three factorizing algorithms in one file

--- Factors.hs
+++ Factors.hs
@@ -13,6 +13,9 @@ module Factors
 , sort3
 , rgs
 , resolv
+, factP
+, factP'
+, sqfof
 ) where
 
 import Data.Bits
@@ -216,6 +219,91 @@ sols n l i j c = do
 -- resolved matrix
 resolv :: [(Integer,[Integer],Integer)] -> Integer -> [Integer] -> [(Integer,Integer)]
 resolv l m bl = [rsv x m | x <- l, let rsv x m = (p,q) where p = gcd (abs(firi(x) - r)) m; r = fexp m bl (seci(x)) 1; q = m `div` p]
+
+
+-- SQFOF algoritm
+sqfof n = do
+  let k = [1,2,3,5,7,11,13,17,19] -- ,3*5,3*7,3*11,5*7,5*11,7*11,3*5*11,3*7*11,3*5*7*11
+      s = sqrt (fromIntegral n)
+      l = 3*floor(sqrt 2*s)
+      p = radix ((head k)*n) 2 0
+      q = ((head k)*n)-p^2
+      i = 2
+      res = sqfr n k l p (p,p) (1,q) i
+  print res
+      
+      
+sqfr :: Integer -> [Integer] -> Integer -> Integer -> (Integer,Integer) -> (Integer,Integer) -> Integer -> (Integer,Integer,Integer)
+sqfr n [] l p0 p q i = (0,0,i)
+sqfr n k l p0 p q i = if (i < l)
+  then do 
+    let b = (p0+snd(p)) `div` snd(q)
+        p1 = b*snd(q) - snd(p)
+        qp = snd(q)
+        q1 = fst(q) + b*(snd(p) - p1)
+    if (mod i 2) == 1
+      then sqfr n k l p0 (snd(p),p1) (qp,q1) (i+1)
+      else if (isSqre q1)
+       then do
+        let r = radix q1 2 0
+            qp2 = r  -- if (mod r 2) == 0 then r `div` 2 else
+            b1 = (p0 - p1) `div` qp2
+            p2 = b1*qp2 + p1
+            pp1 = p2
+            q2 = ((head k)*n - pp1^2) `div` qp2
+        sqfofr k n p0 p2 pp1 qp2 q2
+       else sqfr n k l p0 (snd(p),p1) (qp,q1) (i+1) 
+  else sqfr n (tail k) l p01 (p01,p01) (1,(kn-(p01^2))) 2 
+  where
+    p01 = radix ((head k)*n) 2 0
+    kn = (head k)*n
+    
+    
+sqfofr :: [Integer] -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> (Integer,Integer,Integer)
+sqfofr k n p0 p pp qp q = do
+  let b = (p0+p) `div` q
+      pp1 = p
+      p1 = b*q - p
+      qp1 = q
+      q1 = qp + b*(pp1 - p1)
+  if (p1 /= pp1)
+    then sqfofr k n p0 p1 pp1 qp1 q1
+    else do 
+      let g = gcd n qp1
+      if g == 1
+        then do
+          let k1 = tail k
+          if k1 == []
+            then (0,0,0)
+            else do
+              let pn = radix ((head k1)*n) 2 0
+              let qn = ((head k1)*n)-pn^2
+              sqfr n k1 (3*floor(sqrt 2*sqrt (fromIntegral n))) pn (pn,pn) (1,qn) 2
+        else (g,n `div` (g),head k)
+-- END SQFOF
+
+-- Pollard p-1 factorization function. k starts from 2 incremented by 1 each step, f = k!
+factP :: Integer -> Integer -> Integer -> (Integer,Integer,Integer)
+factP n k f = if (g == n)
+  then (0,0,k) 
+  else if g == 1
+    then factP n (k+1) ((k*f) `mod` n)
+    else (g,n `div` g,k)
+  where 
+    r = (2^k `mod` n)*(2^f `mod` n)
+    g = gcd (r-1) n
+    
+factP' :: Integer -> [Integer] -> Integer -> (Integer,Integer,Integer)
+factP' n [] s = (0,0,s)
+factP' n k s = if g == 1 || g == n
+  then factP' n (tail k) ((s*m) `mod` n)
+  else (g,n `div` g,h)
+  where
+   h = head k
+   lgk = floor(logBase(fromIntegral h) (fromIntegral(n)))
+   m = s*lgk*h
+   r = (2^s `mod` n)*(2^lgk `mod` n)*(2^h `mod` n) - 1
+   g = gcd r n
 
 
  
---
+++ factorize.hs
@@ -0,0 +1,106 @@
+-- Factorization by Pollard p-1 algorithm, squfof, quadratic sieve
+import Factors
+import System.Random
+import Data.Time
+
+
+findPrime p = if (testp p) == [0] then p else findPrime (p+2)
+
+testp :: Integer -> [Integer]
+testp p = [testa [x | b <- [2,3,5,7,11,13,17,19,23,31,37,41,53,61], let x = (powm b (p `div` 2) p 1)^2 `mod` p, x > 1]]
+
+-- test if the result from testp was empty list in which case return 0, else head[xs]
+testa :: [Integer] -> Integer
+testa []     = 0
+testa xs = head xs
+
+
+getopt = do 
+  putStr "1) gimme semi-prime then run k!\n2) gimme semi-prime then run B primes\n3) gen semi-prime then run k!\n"
+  putStr "4) gen semi-prime then run B primes\n5) gimme semi-prime then run sqfof\n6) gen semi-prime then run sqfof\n"  
+  putStrLn "7) gimme semi-prime then run qs\n8) gen semi-prime then run qs"
+  s <-getLine
+  if s == "1" || s == "2" || s == "5" || s == "7"
+    then do 
+      putStrLn "Gimme semi-prime"
+      inp <- getLine
+      let n = read(inp)
+      return (n,s)
+    else if s /= "3" && s /= "4" && s /= "6" && s /= "8"
+      then getopt
+      else do
+       putStrLn "Decimal digits for the two primes to be generated?"
+       inp <- getLine
+       let dig = read(inp)   
+       let b1 = 10^(dig-1)
+       let b2 = 9*10^(dig-1)
+       nu <- randomRIO(b1, b2)
+       let p = if nu  `mod` 2 == 1 then nu else nu + 1
+       let fact1 = findPrime p
+       nu <- randomRIO(b1, b2)
+       let q =  if nu `mod` 2 == 1 then nu else nu + 1
+       let fact2 = findPrime q
+       let n = fact1*fact2
+       return (n,s)
+       
+       
+main = do
+ opt <- getopt
+ let n = fst(opt)
+ let ty = snd(opt)
+ putStr "Semi prime: "
+ print (show n)
+ start <- getCurrentTime
+ if ty == "1" || ty == "3"
+   then do 
+     let pq = factP n 2 1
+     print pq
+   else if ty == "2" || ty == "4" 
+     then do 
+       let primes = fndPrimes [2..floor(8*sqrt (fromIntegral n))] [] 
+       let pq = factP' n primes 1
+       print pq
+       end <- getCurrentTime
+       putStr "Factors found in "
+       print (diffUTCTime end start)
+     else if ty == "5" || ty == "6" 
+       then do 
+         sqfof n
+         end <- getCurrentTime
+         putStr "Factors found in "
+         print (diffUTCTime end start)
+       else do 
+         let b = log (fromIntegral n)
+         let f = if n < 10^9 then 0.63 else if n < 10^13 then 0.58 else if n < 10^16 then 0.56 else if n < 10^19 then 0.52 else 0.51
+         let sqb = f * sqrt (b)
+         let lgb = sqrt (log (b))
+         let bsz = round (exp (sqb * lgb))
+         putStr "Max B: "
+         print bsz
+         let bp = filtB (fndPrimes [2..bsz] []) n
+         putStrLn ("B factors len: "++(show (length bp)))
+         print bp
+         let r = radix n 2 0
+         let scn = scany n r bp 1 (length bp) []
+         putStrLn "Scanned sieve:"
+         print (length(scn))
+         siev <- getCurrentTime
+         putStr "Scan done in "
+         print (diffUTCTime siev start)
+         let ln = toInteger(length bp)
+         let p2 = pow2 (ln-1) []
+         let bf = getBl scn p2 []
+         let rs = replE scn bf []
+         let srt3 = sort3 rs (0,[0],(-1)) []
+         let sol = rgs n srt3 0 (length(scn)-1) (length bp) (length(scn)-1)
+         putStr ("N. solutions: ")
+         print (length sol)
+         red <- getCurrentTime
+         putStr "Matrix reduced in "
+         print (diffUTCTime red siev)
+         let rslt = (resolv sol n bp)
+         putStr "found (p,q): "
+         print rslt 
+         end <- getCurrentTime
+         putStr "Factors found in "
+         print (diffUTCTime end start)
